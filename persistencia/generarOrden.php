2def1b7bd130f8c77729e874a157920edee2ad06
Felipe <pipeta217@gmail.com>
2024-08-24 17:01:31 -0300

    Archivos para generar la orden

---
+++ persistencia/generarOrden.php
@@ -0,0 +1,38 @@
+<?php 
+header('Content-Type: application/json');
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "producto";
+
+$conexion = new mysqli($server, $user, $pass, $db);
+
+if ($conexion->connect_errno) {
+    die("Conexión fallida: " . $conexion->connect_errno);
+} else {
+    obtenerDatos($conexion);
+}
+
+function obtenerDatos($conexion) {   
+    // Obtener los datos de la solicitud POST
+    $data = json_decode(file_get_contents('php://input'), true);
+    $idCarrito = $data['idCarrito'];
+
+    
+
+    // Consulta SQL para eliminar un registro
+    $sql = "UPDATE carrito SET estadoCarrito = 'Confirmado' WHERE idCarrito = $idCarrito";
+
+    // Ejecutar la consulta
+    if (mysqli_query($conexion, $sql)) {
+        // Consulta exitosa
+        echo json_encode(['success' => true]);
+    } else {
+        // Error en la consulta
+        echo json_encode(['success' => false, 'error' => mysqli_error($conexion)]);
+    }
+
+    // Cerrar la conexión
+    mysqli_close($conexion);
+}
+?>
